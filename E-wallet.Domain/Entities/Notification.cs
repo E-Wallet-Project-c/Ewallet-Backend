d8b8642edf92fba3a50f98cc16da1c30d1925498
Enas Tailakh <enastailakh442@gmail.com>
2025-11-14 18:00:12 +0300

    Create new Migrations

--- E-wallet.Domain/Entities/Notification.cs
+++ E-wallet.Domain/Entities/Notification.cs
@@ -10,6 +10,7 @@ namespace E_wallet.Domain.Entities;
 public partial class Notification
 {
     [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
     public int UserId { get; set; }
---
+++ E-wallet.Infrastrucure/Migrations/20251114145731_FixNotificationIdIdentity.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace E_wallet.Infrastrucure.Migrations
+{
+    public partial class FixNotificationIdIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // تعديل عمود Id ليصبح Identity
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Notification",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // التراجع عن التعديل إذا تم Rollback
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Notification",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+    }
+}
